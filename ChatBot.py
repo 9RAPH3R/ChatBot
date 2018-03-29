5c23810f3e3b5c65462ae9d5a93c0dfbee71d1ee
9RAPH3R <33525627+9RAPH3R@users.noreply.github.com>
2018-03-29 17:17:56 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -14,43 +14,43 @@ import os
 class TkinterGUIExample(tk.Tk):
 
     def __init__(self, *args, **kwargs):
-        """
-        Create & set window variables.
-        """
+        
+        """Create & set window variables."""
+        
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.chatbot = ChatBot(
-            "GUI Bot", system_performance_warning=True,
+            "GUI Bot", system_performance_warning=True,         
             storage_adapter="chatterbot.storage.SQLStorageAdapter",
             logic_adapters=[
                 "chatterbot.logic.BestMatch","chatterbot.logic.TimeLogicAdapter","chatterbot.logic.MathematicalEvaluation"
             ],
             input_adapter="chatterbot.input.VariableInputTypeAdapter",
             output_adapter="chatterbot.output.OutputAdapter",
-            #database="./database.db"
+            database="./database.db"
         )
         
-        self.title("ChatBot")
+        self.title("ChatBot")           #Name of the bot on the window
 
         self.initialize()
         
-        style = ttk.Style()
-        style.theme_use('winnative')
-        self.chatbot.set_trainer(ListTrainer)
+        style = ttk.Style()             #For styling the window
+        style.theme_use('winnative')    #winnative is a style
+        
+        self.chatbot.set_trainer(ListTrainer)   
         
         for files in os.listdir('C:/Users/Havilah\Desktop\Product\Completed\chatterbot-corpus-master\chatterbot_corpus\data\english/'):
             data = open( 'C:/Users/Havilah\Desktop\Product\Completed\chatterbot-corpus-master\chatterbot_corpus\data\english/'+ files , 'r').readlines()
             self.chatbot.train(data)
 
     def initialize(self):
-        """
-        Set window layout.
-        """
+        
+        """ Setting the window layout."""
+        
         self.grid()
         
         self.respond = ttk.Button(self, text='Get Response', command=self.get_response)
         self.respond.grid(column=1, row=0, sticky='nesw', padx=3, pady=3)
-
         
         self.usr_input = ttk.Entry(self, state='normal')
         self.usr_input.grid(column=0, row=0, sticky='nesw', padx=3, pady=3)
@@ -63,16 +63,14 @@ class TkinterGUIExample(tk.Tk):
 
         self.respond = ttk.Button(self, text='More>>>', command=self.get_response)
         self.respond.grid(column=1, row=3, sticky='nesw', padx=3, pady=3)
-
-        photo = PhotoImage(file="microphone.png")
         
-        self.respond = ttk.Button(self, image=photo,text = 'Voice', compound=LEFT,command=self.get_response)
+        self.respond = ttk.Button(self, text = 'Voice', compound=LEFT,command=self.get_response)
         self.respond.grid(row=0, column=2 , sticky='nesw', padx=1, pady=1)
 
     def get_response(self):
-        """
-        Get a response from the chatbot and display it.
-        """
+        
+        """Getting response from the chatbot and displaying it."""
+        
         message = self.usr_input.get()
         self.usr_input.delete(0, tk.END)
 
@@ -83,7 +81,7 @@ class TkinterGUIExample(tk.Tk):
         tk.END, "Human: " + message + "\n" + "Bot: " + str(response.text) + "\n")
         self.conversation['state'] = 'disabled'
 
-        time.sleep(0.1)
+        time.sleep(0.1) 
 
 
-TkinterGUIExample().mainloop()
+TkinterGUIExample().mainloop()  #Runs the main class 
